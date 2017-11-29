bac2f9fe49d5c806da597795fd2838e4d03903fd
jake <jh@discordapp.com>
2017-11-29 15:11:45 -0800

    clean up tests.

--- test/ets_hash_ring_test.exs
+++ test/ets_hash_ring_test.exs
@@ -2,25 +2,23 @@ defmodule ETSHashRingTest do
   use ExUnit.Case
   alias HashRingTest.Support.Harness
 
-  def name_for(num_replicas) do
-    :"HashRingETSTest.Replicas#{num_replicas}"
-  end
-
   setup_all do
-    for num_replicas <- Harness.replicas() do
-      {:ok, _pid} = HashRing.ETS.start_link(name_for(num_replicas), Harness.nodes(), num_replicas)
+    rings = for num_replicas <- Harness.replicas(), into: %{} do
+      name = :"HashRingETSTest.Replicas#{num_replicas}"
+      {:ok, _pid} = HashRing.ETS.start_link(name, Harness.nodes(), num_replicas)
+      {num_replicas, name}
     end
-    :ok
+    {:ok, rings: rings}
   end
 
   for num_replicas <- Harness.replicas() do
     describe "ets hash ring, replicas=#{num_replicas}" do
       for key <- Harness.keys() do
-        test "find_node key=#{key}" do
-          assert HashRing.ETS.find_node(name_for(unquote(num_replicas)), unquote(key)) == Harness.find_node(unquote(num_replicas), unquote(key))
+        test "find_node key=#{key}", %{rings: rings} do
+          assert HashRing.ETS.find_node(rings[unquote(num_replicas)], unquote(key)) == Harness.find_node(unquote(num_replicas), unquote(key))
         end
-        test "find_nodes key=#{key} num=#{Harness.num()}" do
-          assert HashRing.ETS.find_nodes(name_for(unquote(num_replicas)), unquote(key), Harness.num()) == Harness.find_nodes(unquote(num_replicas), unquote(key), Harness.num())
+        test "find_nodes key=#{key} num=#{Harness.num()}", %{rings: rings} do
+          assert HashRing.ETS.find_nodes(rings[unquote(num_replicas)], unquote(key), Harness.num()) == Harness.find_nodes(unquote(num_replicas), unquote(key), Harness.num())
         end
       end
     end
