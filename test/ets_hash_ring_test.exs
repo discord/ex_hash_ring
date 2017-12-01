cbc80cce242efde1bd1e9d50c6ba0eddba905d92
jake <jh@discordapp.com>
2017-12-01 15:08:05 -0800

    cleanup tests w/ setup function.

--- test/ets_hash_ring_test.exs
+++ test/ets_hash_ring_test.exs
@@ -30,82 +30,92 @@ defmodule ETSHashRingTest do
       end
     end
   end
+end
 
-  test "construct with nodes" do
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.ConstructWithNodes, Harness.nodes())
-    {:ok, nodes} = Ring.get_nodes(pid)
-    assert nodes == Harness.nodes()
+defmodule ETSHAshRingOperationsTest do
+  use ExUnit.Case
+  alias HashRing.ETS, as: Ring
+  alias HashRing.ETS.Config
+
+  setup_all do
+    Config.start_link()
+    :ok
   end
 
-  test "set nodes" do
-    new_nodes = ["a", "b", "c"]
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.SetNodes, Harness.nodes())
-    {:ok, _} = Ring.set_nodes(pid, new_nodes)
-    {:ok, nodes} = Ring.get_nodes(pid)
+  @nodes ["a", "b", "c"]
+
+  setup do
+    name = :"HashRingEtsTest#{:erlang.unique_integer([:positive])}"
+    {:ok, pid} = Ring.start_link(name, @nodes)
+#    on_exit fn -> Ring.stop(pid) end
+
+    [name: name]
+  end
+
+  test "construct with nodes", %{name: name} do
+    {:ok, nodes} = Ring.get_nodes(name)
+    assert nodes == @nodes
+  end
+
+  test "set nodes", %{name: name} do
+    new_nodes = ["d", "e", "f"]
+    {:ok, _} = Ring.set_nodes(name, new_nodes)
+    {:ok, nodes} = Ring.get_nodes(name)
     assert nodes == new_nodes
 
     # Assert that the ring is also re-generated at this point.
-    assert Ring.get_ring_gen(HashRingEtsTest.SetNodes) == {:ok, 2}
-    assert Ring.find_node(HashRingEtsTest.SetNodes, 1) in new_nodes
+    assert Ring.get_ring_gen(name) == {:ok, 2}
+    assert Ring.find_node(name, 1) in new_nodes
   end
 
-  test "add node" do
-    nodes = ["a", "b"]
-    expected_nodes = ["c" | nodes]
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.AddNode, nodes)
-    {:ok, _} = Ring.add_node(pid, "c")
-    {:ok, ^expected_nodes} = Ring.get_nodes(pid)
+  test "add node", %{name: name} do
+    expected_nodes = ["d" | @nodes]
+    {:ok, _} = Ring.add_node(name, "d")
+    {:ok, ^expected_nodes} = Ring.get_nodes(name)
     # Select a node that should now be c.
-    assert Ring.get_ring_gen(HashRingEtsTest.AddNode) == {:ok, 2}
-    assert Ring.find_node(HashRingEtsTest.AddNode, 1) == "c"
+    assert Ring.get_ring_gen(name) == {:ok, 2}
+    assert Ring.find_node(name, 1) == "c"
   end
 
-  test "remmove node" do
-    nodes = ["a", "b", "c"]
-    expected_nodes = nodes -- ["c"]
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.RemoveNode, nodes)
-    {:ok, _} = Ring.remove_node(pid, "c")
-    {:ok, ^expected_nodes} = Ring.get_nodes(pid)
+  test "remmove node", %{name: name} do
+    expected_nodes = @nodes -- ["c"]
+    {:ok, _} = Ring.remove_node(name, "c")
+    {:ok, ^expected_nodes} = Ring.get_nodes(name)
     # Select a node that should now be b.
-    assert Ring.get_ring_gen(HashRingEtsTest.RemoveNode) == {:ok, 2}
-    assert Ring.find_node(HashRingEtsTest.RemoveNode, 1) == "b"
+    assert Ring.get_ring_gen(name) == {:ok, 2}
+    assert Ring.find_node(name, 1) == "b"
   end
 
-  test "ets config will remove config" do
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.ProcessWillDie, Harness.nodes())
-    assert Config.get(HashRingEtsTest.ProcessWillDie) != :error
-    assert Ring.stop(pid) == :ok
-    assert await(fn -> Config.get(HashRingEtsTest.ProcessWillDie) == :error end)
+  test "ets config will remove config", %{name: name} do
+    assert Config.get(name) != :error
+    assert Ring.stop(name) == :ok
+    assert await(fn -> Config.get(name) == :error end)
   end
 
-  test "ring gen gc happens" do
-    nodes = ["a", "b", "c"]
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.ManualRingGenGc, nodes)
-    {:ok, _} = Ring.remove_node(pid, "c")
-    assert Ring.get_ring_gen(HashRingEtsTest.ManualRingGenGc) == {:ok, 2}
+  test "ring gen gc happens", %{name: name} do
+    {:ok, _} = Ring.remove_node(name, "c")
+    assert Ring.get_ring_gen(name) == {:ok, 2}
 
 
-    assert Ring.force_gc(pid, 1) == :ok
-    assert Ring.force_gc(pid, 1) == {:error, :not_pending}
+    assert Ring.force_gc(name, 1) == :ok
+    assert Ring.force_gc(name, 1) == {:error, :not_pending}
 
     # Break the veil and look under the hood and make sure that we don't have any old things in it anymore.
-    assert count_ring_gen_entries(HashRingEtsTest.ManualRingGenGc, 1) == 0
-    assert ring_ets_table_size(HashRingEtsTest.ManualRingGenGc) == 1024
+    assert count_ring_gen_entries(name, 1) == 0
+    assert ring_ets_table_size(name) == 1024
   end
 
-  test "automatic ring gc" do
+  test "automatic ring gc", %{name: name} do
     Application.put_env(:hash_ring, :ets_gc_delay, 50)
     on_exit fn -> Application.delete_env(:hash_ring, :ets_gc_delay) end
 
-    nodes = ["a", "b", "c"]
-    {:ok, pid} = Ring.start_link(HashRingEtsTest.AutomaticRingGc, nodes)
-    {:ok, _} = Ring.remove_node(pid, "c")
-    assert Ring.get_ring_gen(HashRingEtsTest.AutomaticRingGc) == {:ok, 2}
+    {:ok, _} = Ring.remove_node(name, "c")
+    assert Ring.get_ring_gen(name) == {:ok, 2}
 
     # Break the veil and look under the hood and make sure that we don't have any old things in it anymore.
-    assert await(fn -> count_ring_gen_entries(HashRingEtsTest.AutomaticRingGc, 1) == 0 end)
-    assert Ring.force_gc(pid, 1) == {:error, :not_pending}
-    assert ring_ets_table_size(HashRingEtsTest.AutomaticRingGc) == 1024
+    assert await(fn -> count_ring_gen_entries(name, 1) == 0 end)
+    assert Ring.force_gc(name, 1) == {:error, :not_pending}
+    assert ring_ets_table_size(name) == 1024
   end
 
   test "operations on nonexistent ring dont fail" do
