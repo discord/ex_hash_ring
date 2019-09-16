ea8f4c06f617bb54e471c0312c834365ae55cb0f
oceans <sea@likeanocean.com>
2019-09-16 03:51:13 -0700

    version bump -> 4.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExHashRing.HashRing.Mixfile do
   def project do
     [
       app: :ex_hash_ring,
-      version: "3.0.0",
+      version: "4.0.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
